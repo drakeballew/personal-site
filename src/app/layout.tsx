26221388034013c6a4bdab4c69b03109ec6e1a1b
Drake Ballew <drake@pacaya.io>
2024-10-16 17:59:05 -0700

    Add Inter font and SimpleAnalytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <head>
+      <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+      </head>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ export default {
   darkMode: 'selector',
   plugins: [typographyPlugin],
   theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
     fontSize: {
       xs: ['0.8125rem', { lineHeight: '1.5rem' }],
       sm: ['0.875rem', { lineHeight: '1.5rem' }],
